ca3fab65508f3750b50d5cd824b328b7f661f317
Mandar007007 <teamm2d07@gmail.com>
2024-06-14 16:21:42 +0530

    error handlin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,26 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Home from './components/Home'
 import LoadScreen from './components/LoadScreen';
 import GameScreen from './components/GameScreen';
+import { socket } from './socket';
 
 function App() {
+
+  socket.on('connect', () => {
+    console.log(`Connected: ${socket.id}`);
+  });
+  
+  socket.on('disconnect', (reason) => {
+    console.log(`Disconnected: ${reason}`);
+  });
+  
+  socket.on('connect_error', (error) => {
+    console.error(`Connect error: ${error}`);
+  });
+  
+  socket.on('error', (error) => {
+    console.error(`Socket error: ${error}`);
+  });
+  
 
   return (
     <>
