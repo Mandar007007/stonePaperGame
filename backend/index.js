ed25afd62e7c33b5dc5f50587c15e116cad0a40e
Mandar007007 <teamm2d07@gmail.com>
2024-06-14 16:14:05 +0530

    socket

--- backend/index.js
+++ backend/index.js
@@ -106,7 +106,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
